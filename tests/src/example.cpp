5115ff3885ed80c53082ecc0dbe962859425dd3b
Patrick Cox <coxpatrick74@gmail.com>
2019-05-27 15:52:47 -0400

    Split test_deploy and example.cpp

---
+++ tests/src/example.cpp
@@ -0,0 +1,32 @@
+#include <basicppmsa/InterruptHandler.hpp>
+#include <basicppmsa/MainServiceController.hpp>
+#include <basicppmsa/RuntimeUtils.hpp>
+
+int main()
+{
+    cppmsa::InterruptHandler::hookSIGINT();
+
+    cppmsa::MainServiceController server;
+    server.setEndpoint("http://host_auto_ip4:6502/cppmsa/api");
+
+    try
+    {
+        server.accept().wait();
+        std::cout << "C++ Microservice now listening for requests at: "
+                  << server.endpoint() << std::endl;
+
+        cppmsa::InterruptHandler::waitForUserInterrupt();
+
+        server.shutdown().wait();
+    }
+    catch (std::exception const & e)
+    {
+        std::cerr << "Error: " << std::endl;
+    }
+    catch (...)
+    {
+        cppmsa::RuntimeUtils::printStackTrace();
+    }
+
+    return 0;
+}
--- tests/src/test_deploy.cpp
+++ tests/src/test_deploy.cpp
@@ -2,31 +2,11 @@
 #include <basicppmsa/MainServiceController.hpp>
 #include <basicppmsa/RuntimeUtils.hpp>
 
-int main(int argc, const char * argv[])
+int main()
 {
     cppmsa::InterruptHandler::hookSIGINT();
 
     cppmsa::MainServiceController server;
-    server.setEndpoint("http://host_auto_ip4:6502/cppmsa/api");
-
-    try
-    {
-        server.accept().wait();
-        std::cout << "C++ Microservice now listening for requests at: "
-                  << server.endpoint() << std::endl;
-
-        cppmsa::InterruptHandler::waitForUserInterrupt();
-
-        server.shutdown().wait();
-    }
-    catch (std::exception const & e)
-    {
-        std::cerr << "Error: " << std::endl;
-    }
-    catch (...)
-    {
-        cppmsa::RuntimeUtils::printStackTrace();
-    }
 
     return 0;
 }

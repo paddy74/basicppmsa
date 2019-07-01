3cb763402ac4709dba9d6b7e579b70ce7ab689da
Patrick Cox <coxpatrick74@gmail.com>
2019-07-01 15:10:57 -0400

    Add inline comments to example.cpp

--- samples/src/example.cpp
+++ samples/src/example.cpp
@@ -4,19 +4,25 @@
 
 int main()
 {
+    // Hook on the SIGINT interrupt
     basicppmsa::InterruptHandler::hookSIGINT();
 
+    // Create and initialize the service at the endpoint
     basicppmsa::MainServiceController server;
     server.setEndpoint("http://host_auto_ip4:6502/basicppmsa/api");
 
     try
     {
+        // Initialize the thread pool used by pplx and start accepting network
+        //  connections
         server.accept().wait();
         std::cout << "C++ Microservice now listening for requests at: "
                   << server.endpoint() << std::endl;
 
+        // Prevent the process from leaving until SIGINT
         basicppmsa::InterruptHandler::waitForUserInterrupt();
 
+        // Shutdown the service and wait for all threads to finish
         server.shutdown().wait();
     }
     catch (std::exception const & e)
